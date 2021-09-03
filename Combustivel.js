e56ccca91e3dcf66a5e12b994b5161c4a84b3dff
Wellington <wellington.multilaser@gmail.com>
2021-09-03 15:23:44 -0300

    troca do console.log por alert

--- Combustivel.js
+++ Combustivel.js
@@ -2,8 +2,10 @@ let etanol = parseInt(prompt("Qual valor do etanol? "));
 let gasolina = parseInt(prompt("Qual valor da gasolina? "));
 let resultado = gasolina * 0.70;
 if (resultado > etanol){
-    console.log("Melhor usar Etanol")
+    alert("Melhor usar etanol")
 }
 else {
-    console.log("Melhor Usar Gasolina")
+    alert("Melhor usar gasolina")
 }
+
+/*console.log("Melhor Usar Gasolina")*/
