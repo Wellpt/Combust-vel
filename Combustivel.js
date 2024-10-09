d3a819126cea75e1f08bb5935de3bad094a5aae1
Wellington Nobre <Wellington.quaresma@oi.net.br>
2024-10-09 16:47:40 -0300

    Refatoracao  do codigo para algo mais novo

--- Combustivel.js
+++ Combustivel.js
@@ -1,11 +1,28 @@
-let etanol = parseInt(prompt("Qual valor do etanol? "));
-let gasolina = parseInt(prompt("Qual valor da gasolina? "));
-let resultado = gasolina * 0.70;
-if (resultado > etanol){
-    alert("Melhor usar etanol")
-}
-else {
-    alert("Melhor usar gasolina")
+//CODIGO REFATORADO
+
+function checkeMelhorCombustivel (gasolina, etanol) {
+    const result = gasolina * 0.70
+    if ( result > etanol) {
+        alert("Melhor usar etanol")
+    }else {
+        alert("Melhor usar gasolina")
+    }
 }
 
-/*console.log("Melhor Usar Gasolina")*/
+const etanol = parseInt(prompt("Qual valor do etanol? "), 10)
+const gasolina = parseInt(prompt("Qual valor do gasolina? "), 10)
+
+checkeMelhorCombustivel(gasolina, etanol)
+
+
+//CODIGO ANTIGO
+
+// let etanol = parseInt(prompt("Qual valor do etanol? "));
+// let gasolina = parseInt(prompt("Qual valor da gasolina? "));
+// let resultado = gasolina * 0.70;
+// if (resultado > etanol){
+//     alert("Melhor usar etanol")
+// }
+// else {
+//     alert("Melhor usar gasolina")
+// }
